1eeeca4c8ad1539915e4fcec211ea8a50cf0d2e2
keetha805 <Ketha508@gmail.com>
2024-01-19 18:47:23 +0100

    ERC4907 and IERC4907 contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@openzeppelin/hardhat-upgrades");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: {},
+  },
   solidity: "0.8.19",
 };
