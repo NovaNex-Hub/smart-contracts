8b7acaa6900a5046a59addb3e6ecba0175f98ad1
max-clinch <74224236+max-clinch@users.noreply.github.com>
2024-01-29 23:41:30 +0100

    first commit

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,24 +4,45 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+//const hre = require("hardhat");
+//
+//async function main() {
+//  const feeAccount = (await hre.getNamedAccounts()).deployer;
+//  const feePercentage = 15;
+//  const marketplace = await hre.ethers.deployContract("NovaNexHubMarketplace", [
+//    feeAccount,
+//    feePercentage,
+//  ]);
+//
+//  await marketplace.waitForDeployment();
+//
+//  console.log("marketplace: ", marketplace);
+//}
+//
+//// We recommend this pattern to be able to use async/await everywhere
+//// and properly handle errors.
+//main().catch((error) => {
+//  console.error(error);
+//  process.exitCode = 1;
+//});
+//
+
+const { ethers } = require("hardhat");
 
 async function main() {
-  const feeAccount = (await hre.getNamedAccounts()).deployer;
-  const feePercentage = 15;
-  const marketplace = await hre.ethers.deployContract("NovaNexHubMarketplace", [
-    feeAccount,
-    feePercentage,
-  ]);
+  const ERC4907 = await ethers.getContractFactory("ERC4907");
+  const erc4907 = await ERC4907.deploy();
+  await erc4907.deployed();
 
-  await marketplace.waitForDeployment();
+  const NovaNexHub = await ethers.getContractFactory("NovaNexHub");
+  const novaNexHub = await NovaNexHub.deploy();
+  await novaNexHub.deployed();
 
-  console.log("marketplace: ", marketplace);
+  const NovaNexHubMarketplace = await ethers.getContractFactory("NovaNexHubMarketplace");
+  const nft = await NovaNexHubMarketplace.deploy();
+  await nft.deployed();
+
+  console.log("Contracts deployed:", erc4907.address, novaNexHub.address, nft.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main();
